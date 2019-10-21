8599cd77dfe4e08a5f25806359e8a6868b07b156
Goldenstriker <ndparab240@gmail.com>
2019-10-21 19:33:55 +0530

    1 file modified

--- app/_helpers/jwt.interceptor.ts
+++ app/_helpers/jwt.interceptor.ts
@@ -1,27 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from "@angular/common/http";
+import { Observable } from "rxjs";
 
-import { AuthenticationService } from '../_services';
+import { AuthenticationService } from "../_services";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add authorization header with jwt token if available
-        let currentUser = this.authenticationService.currentUserValue;
-        if (currentUser && currentUser.token) {
-            console.log(currentUser);
-            request = request.clone({
-                /*setHeaders: { 
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // add authorization header with jwt token if available
+    let currentUser = this.authenticationService.currentUserValue;
+    if (currentUser && currentUser.token) {
+      console.log(currentUser);
+      request = request.clone({
+        /*setHeaders: { 
                     Authorization: `Bearer ${currentUser.token}`
                 }*/
-                headers: request.headers.set('Authorization', `Bearer ${currentUser.token}`)
-            });
-            console.log(request.headers);
-        }
-
-        return next.handle(request);
+        setHeaders: {
+              "Content-Type": "application/json; charset=utf-8",
+              'Accept': "application/json",
+              'Authorization': `Bearer ${currentUser.token)}`
+            }
+      });
+      console.log(request.headers);
     }
+
+    return next.handle(request);
+  }
 }
