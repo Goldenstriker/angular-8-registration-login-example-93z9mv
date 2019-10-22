4da286972b32e8bf390cfd34b70c5a69ea01c9ee
Goldenstriker <ndparab240@gmail.com>
2019-10-22 11:53:06 +0530

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.userService.getCurrentLoggedIn().subscribe(x => {
       this.currentLoggedInUser = x.user;
+      console.log(x.user);
     });
   }
 
