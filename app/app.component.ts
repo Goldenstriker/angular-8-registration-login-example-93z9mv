9b5420905d4bc8fe38aa24d8163a34742d2952af
Goldenstriker <ndparab240@gmail.com>
2019-10-22 11:05:40 +0530

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthenticationService, UserService } from "./_services";
+import { User } from "./_models";
 
-import { AuthenticationService } from './_services';
-import { User } from './_models';
-
-@Component({ selector: 'app', templateUrl: 'app.component.html' })
+@Component({ selector: "app", templateUrl: "app.component.html" })
 export class AppComponent {
-    currentUser: User;
+  currentUser: User;
+  currentLoggedInUser: string;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      x => (this.currentUser = x)
+    );
+    this.userService
+      .getCurrentLoggedIn()
+      .subscribe(x => (this.currentLoggedInUser = x.username));
+  }
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    }
-
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/login']);
-    }
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(["/login"]);
+  }
 }
